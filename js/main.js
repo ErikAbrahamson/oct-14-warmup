61c1c85ffbbfc0aa35d1a2db3a1d60895778145d
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-10-14 10:33:02 -0600

    specify test bounds

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-/* One Missing: Write a function that takes an array of integers between 1 and 10, with one missing integer, as an argument and returns the missing integer. Test this with Mocha and Chai. */
-
 function missing(array) {
   array.sort(function(a, b) { return a - b; });
   var int, i = 0, j = i + 1;
--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@ var expect = chai.expect;
 var test = [4,2,1,5,9,10,7,6,3];
 var test2 = [1,5,3,2,10,8,6,7,4];
 
-describe('Get missing integer', function() {
-  it('Should find the missing integer', function() {
+describe('Get missing integer from array 1-10', function() {
+  it('Should find the number eight', function() {
     expect(missing(test)).to.equal(8);
   });
-  it('Should find the missing integer', function() {
+  it('Should find the number nine', function() {
     expect(missing(test2)).to.equal(9);
   });
 });
